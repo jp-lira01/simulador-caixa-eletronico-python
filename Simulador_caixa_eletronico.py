8be61c7b5d8b08bd627e0a8c10b029786638782c
João Pedro <271107579+jp-lira01@users.noreply.github.com>
2026-08-05 22:07:51 -0300

    Adiciona simulador de caixa eletrônico

---
+++ Simulador_caixa_eletronico.py
@@ -0,0 +1,20 @@
+print("=" * 35)
+print("        BANCO PYTHON")
+print("=" * 35)
+
+valor = int(input("Digite o valor que deseja sacar: R$ "))
+
+total = valor
+cedulas = [50, 20, 10, 5, 1]
+
+print("\nNotas entregues:")
+
+for cedula in cedulas:
+    quantidade = total // cedula
+    
+    if quantidade > 0:
+        print(f"{quantidade} nota(s) de R$ {cedula}")
+        total -= quantidade * cedula
+
+print("=" * 35)
+print("Obrigado por usar o Banco Python!")
